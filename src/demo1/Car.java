f27139fbfd814a5d8b5a3f18d4143dce79bdbf56
davila1292 <daap1292@gmail.com>
2025-01-30 21:35:32 -0600

    Update Car.java
    added showAcceleration()

--- src/demo1/Car.java
+++ src/demo1/Car.java
@@ -1,11 +1,15 @@
 package demo1;
 
 public class Car extends Vehicle {
-	
+	int acceleration = 0;
 	@Override
 	public void accelerate() {
 		System.out.println("Car is moving");
+		acceleration = acceleration + 10;
 	}
-	
+
+	public void showAcceleration(){
+		System.out.println(acceleration);
+	}
 
 }
